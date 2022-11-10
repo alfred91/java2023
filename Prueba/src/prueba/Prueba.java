0c7b5dd1caabc3037963cff896e5cdaf896e9950
usario1daw <alfred.91.mc@gmail.com>
2022-11-10 21:52:23 +0100

    prueba1

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -3,7 +3,25 @@ package prueba;
 public class Prueba {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo !"); 
-	}
+		
+	
+short num1=8;
+int mes=12;
+long distancia=300000;
 
+char letra='a';
+float decimal = 1e10f;
+double precio= 35000.95;
+
+boolean ganasDeIrme = true;
+
+System.out.println(num1);
+System.out.println(mes);
+System.out.println(letra);
+System.out.println(decimal);
+System.out.println(precio);
+System.out.println(ganasDeIrme);
+
+
+	}
 }
