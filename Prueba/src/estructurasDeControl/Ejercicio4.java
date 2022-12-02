6e5cca2866ec63dfe981bbb13c4a37518041ba25
usario1daw <alfred.91.mc@gmail.com>
2022-12-02 18:56:11 +0100

    2

--- Prueba/src/estructurasDeControl/Ejercicio4.java
+++ Prueba/src/estructurasDeControl/Ejercicio4.java
@@ -4,22 +4,72 @@ import java.util.Scanner;
 
 public class Ejercicio4 {
 	
-	public static int billetes(int cinco, int diez, int veinte,
-			int cincuenta, int cien, int doscientos, int quinientos) {
-int cinco=5; int diez=10; int veinte=20; int cincuenta=50;
-int cien=100; int doscientos=200; int quinientos=500;
-int cantidad=0;
-Scanner sc = new Scanner(System.in);
-System.out.println("Introduce una cantidad");
-cantidad = Integer.parseInt(sc.nextLine());
+	public static void billetes(int num) {
+		
+	int euros=num, quinientos=0, doscientos=0,
+		cien=0, cincuenta=0, veinte=0, diez=0, cinco=0,
+		monedas=0, resto=0;
+	
+quinientos=euros/500;
 
-return cantidad; 
-System.out.println();
+if (euros%500!=0) {
+	resto=(euros%500);
+	doscientos=(int)(resto)/200;
+
+	resto=(resto%200);
 }
-
-	public static void main(String[] args) {
-System.out.println(cantidad());
-
+	if (resto!=0) {
+		cien=(int)(resto) /100;
+		
+		resto=(resto%100);
 	}
-
+		if (resto!=0) {
+			cincuenta=(int)(resto)/50;
+			
+			resto=(resto%50);
+			veinte=(int)(resto)/20;
+		}
+		if(resto!=0) {
+			resto=(resto%20);
+			diez=(int)(resto)/10;
+		}
+		if (resto!=0) {
+			resto=(int)(resto%10);
+			cinco=(int)(resto)/5;
+		}
+			if (resto!=0) {
+				resto=(int)(resto%5);
+				monedas=(int)(resto)/1;
+			}
+			
+	System.out.println("> Billetes de 500€: "+quinientos);
+	System.out.println("> Billetes de 200€: "+doscientos);
+	System.out.println("> Billetes de 100€: "+cien);
+	System.out.println("> Billetes de 50€: " + cincuenta);
+	System.out.println("> Billetes de 20€: " + veinte);
+	System.out.println("> Billetes de 10€: " + diez);
+	System.out.println("> Billetes de 5€: " + cinco);
+	System.out.println("> Monedas de 1€: " + monedas);
 }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int error=0; int num=0;
+		
+		do {
+			try { System.out.println("Introduce una cantidad");
+			num = Integer.parseInt(sc.nextLine()); 
+			if(num<=0) {
+				System.out.println("ERROR");
+			} else {
+				break;
+			}
+			} catch (Exception e) {
+				error=1;
+				System.out.println("Error1");
+				System.out.println(e.getMessage());
+			}
+		}while(error ==1 || num<=0);
+		billetes(num);
+		sc.close();
+			}
+	}
---
+++ Prueba/src/estructurasDeControl/Ejercicio5.java
@@ -0,0 +1,13 @@
+package estructurasDeControl;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int error=0; int num=0;
+
+	}
+
+}
