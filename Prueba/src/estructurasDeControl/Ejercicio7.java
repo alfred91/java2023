41cfe913e9041abac7998b601854de7e73a75a82
Apache <alfred.91.mc@gmail.com>
2022-12-04 23:57:03 +0100

    5

--- Prueba/src/estructurasDeControl/Ejercicio7.java
+++ Prueba/src/estructurasDeControl/Ejercicio7.java
@@ -1,10 +1,23 @@
 package estructurasDeControl;
-
-import java.util.Scanner;
-
 public class Ejercicio7 {
-static int sumaimpares=100*100;
-	    public static void main(String[] args) {
-	System.out.println("La suma de los 100 primeros numeros impares es: "+sumaimpares);
-		        }		    
+	
+public static boolean esImpar(int iNumero) {
+	  if (iNumero%2!=0)
+	    return true;
+	  else
+	    return false;
 	}
+    public static void main(String[] args) {
+    	int contador = 0;
+    	int incremento = 1;
+    	while (contador !=100) {	
+    		
+    	  if (esImpar(incremento)) {
+    	    System.out.println(incremento);
+    	    contador++;
+    	  }			
+    	  incremento++;  
+    	}
+    	System.out.println("La suma de los 100 primeros numeros impares es: "+contador*incremento);
+  }
+}
--- Prueba/src/estructurasDeControl/Ejercicio8.java
+++ Prueba/src/estructurasDeControl/Ejercicio8.java
@@ -1,22 +1,24 @@
 package estructurasDeControl;
-
 import java.util.Scanner;
+
 public class Ejercicio8 {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int i;
-        double suma, un_numero;
-        suma = 0;
-        for (i=1; i<=100; i++) {
-            System.out.print("PROCESO " + i);
-            System.out.print("Ingresa el valor de un numero: ");
-            un_numero = in.nextDouble();
-            in.nextLine();
-            suma=suma+un_numero;
-            System.out.println();
-        }
-        System.out.println("Valor de suma: " + suma);
-    }
-
+    	int numero=0;
+    	
+    	Scanner sc = new Scanner(System.in);
+		System.out.println("Dime un numero");
+		numero = Integer.parseInt(sc.nextLine());
+		int dobles=(numero/2), triples=(numero/3);
+		int i=0;
+		
+		System.out.println("Dobles "+dobles);
+		for (i=0;i<=dobles;i++) {
+			if(i!=0&&numero%i==0) {System.out.println(i);}
+		}
+			
+		System.out.println("Triples "+triples);
+		for (i=0;i<=triples;i++) {
+			if(i!=0&&numero%i==0) {System.out.println(i);}
+  }}
 }
