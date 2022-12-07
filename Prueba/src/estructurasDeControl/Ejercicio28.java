d11c5c6ffb18e5a66e86105847c19959f0795fb4
Apache <alfred.91.mc@gmail.com>
2022-12-07 05:23:51 +0100

    071222

--- Prueba/src/estructurasDeControl/Ejercicio28.java
+++ Prueba/src/estructurasDeControl/Ejercicio28.java
@@ -1,10 +1,24 @@
 package estructurasDeControl;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class Ejercicio28 {
 
 	public static void main(String[] args) {
-
-
+		 List<String> list = new ArrayList<>();
+		 
+		 scanner sc=new Scanner(System.in);
+		 System.out.println("Introduce el nombre");
+		 String name = input.nextLine();
+	        list.add(nombre1);
+	        list.add("US");
+	        list.add("China");
+	        list.add("Russia");
+	        list.add("Australia");
+	        System.out.println(list); 
+	        list.sort(String::compareToIgnoreCase);
+	        System.out.println("-----------Sorted List------------");
+	        System.out.println(list);        
 	}
 
 }
