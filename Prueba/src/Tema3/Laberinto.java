24d7161789dd0dd159fb64fd4eb2c512da6bdfe2
Apache <alfred.91.mc@gmail.com>
2022-12-16 00:02:25 +0100

    1612

---
+++ Prueba/src/Tema3/Laberinto.java
@@ -0,0 +1,92 @@
+package Tema3;
+
+import java.util.Scanner;
+public class Laberinto {
+
+	public static void pintarMatriz(char[][] matriz) {
+		
+		for(int i=0; i < matriz.length; i++) {
+			for(int j=0; j < matriz[i].length; j++) {
+				System.out.print(matriz[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int opc=0;
+	char [][] matriz=new char[20][20];
+		
+		for(int i=0; i<matriz.length; i++) {
+			for(int j=0; j<matriz[i].length; j++) {
+				
+				//Ponemos los puntos	
+				matriz[i][j] = '.';
+		
+				//Columna derecha
+				if (j == matriz[i].length - 1) {
+					matriz[i][j] = '|';
+				}
+				
+				//Columna izquierda
+				if (j == 0) {
+					matriz[i][j] = '|';
+				}
+				
+				//Fila abajo
+				if (i == matriz.length - 1) {
+					matriz[i][j] = '-';
+				}
+				
+				//Fila arriba
+				if (i == 0) {
+					matriz[i][j] = '-';
+				}else if (j==19) {
+					matriz[i][j] = '|';
+				}
+				
+				matriz[0][19]='|';
+				matriz[0][0]='@';
+				matriz[19][0]='|';		
+				}
+			}
+			
+		pintarMatriz(matriz);
+
+		System.out.println("Introduce Opcion:");
+		System.out.println("1. Derecha");
+		System.out.println("2. Izquierda");
+		System.out.println("3. Abajo");
+		System.out.println("4. Arriba");
+		try {
+			opc=Integer.parseInt(sc.nextLine());
+		}catch (Exception e) {
+			System.out.println("Dime un numero");
+		}
+		
+		switch(opc){
+		case 1:	
+			matriz[0][0]='|';
+			matriz[0][1]='@';
+			pintarMatriz(matriz);
+		;break;
+			
+		case 2:
+			System.out.println("Has salido del laberinto");
+		;break;
+			
+		case 3: 
+			matriz[0][0]='|';
+			matriz[1][0]='@';
+			pintarMatriz(matriz);
+		;break;
+			
+		case 4: 
+			System.out.println("Has salido del laberinto");
+			pintarMatriz(matriz);
+		;break;				
+		}				
+	}
+
+}
---
+++ Prueba/src/prueba/ejercicio4.java
@@ -0,0 +1,87 @@
+package prueba;
+import java.util.Scanner;
+public class ejercicio4 {
+
+	public static void pintar(char[][] matriz) {
+		for(int i=0; i < matriz.length; i++) {
+			for(int j=0; j < matriz[i].length; j++) {
+				System.out.print(matriz[i][j] + " ");
+				}
+			System.out.println();
+			}
+		}
+	
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+		int opc=0;
+		char  [][] matriz= new char [20][20];
+
+		
+		for(int i=0; i < matriz.length; i++) {
+			for(int j=0; j < matriz[i].length; j++) {
+				
+
+			matriz[i][j] = '.';
+
+			if (j == matriz[i].length - 1) {
+				matriz[i][j] = '|';
+			}
+
+			if (j == 0) {
+				matriz[i][j] = '|';
+			}
+			
+			if (i == matriz.length - 1) {
+				matriz[i][j] = '-';
+			}
+
+			if (i == 0) {
+				matriz[i][j] = '-';
+			}else if (j==19) {
+				matriz[i][j] = '|';
+			}
+			
+			matriz[0][19]='|';
+			matriz[0][0]='@';
+			matriz[19][0]='#';
+		
+			}
+	}		
+		do {
+		pintar(matriz);
+	System.out.println("Introduce Opcion:");
+	System.out.println("1. Izquierda");
+	System.out.println("2. Derecha");
+	System.out.println("3. Arriba");
+	System.out.println("4. Abajo");
+
+	try {
+			opc=Integer.parseInt(sc.nextLine());
+		}catch (Exception e) {
+			System.out.println("Escribe un número (1-4)");
+		}
+		switch(opc){
+
+		case 1:
+			System.out.println("Te has salido de la zona");
+		;break;
+		
+		case 2:	
+			matriz[0][0]='|';
+			matriz[0][1]='@';
+			pintar(matriz);
+		;break;
+		
+		case 3: 
+			System.out.println("Te has salido de la zona");
+			pintar(matriz);
+		;break;
+		
+		case 4: 
+			matriz[0][0]='|';
+			matriz[1][0]='@';
+			pintar(matriz);
+		;break;
+		}} while(opc==1||opc==2||opc==3||opc==4);
+	}
+}
