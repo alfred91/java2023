e38184d2b3648dbc1e92ad930f1bcb6d896e876a
usario1daw <alfred.91.mc@gmail.com>
2022-12-15 21:49:02 +0100

    151222

--- Prueba/src/arraysYmatrices/Ejercicio1.java
+++ Prueba/src/arraysYmatrices/Ejercicio1.java
@@ -5,15 +5,7 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		
 		int i;
-        int[] numeros = new int[10];
-
-        Scanner sc = new Scanner(System.in);
-
-        for(i=0; i<numeros.length; i++)
-        {
-            System.out.printf("Introduzca número "+(i+1)+" : ");
-            numeros[i] = sc.nextInt();
-        }
+        int[] numeros = new int[50];
 
         for(i=numeros.length-1; i>=0;i--)
         {
