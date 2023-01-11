5c874a062431404739f98e0eb949af650d755a61
usario1daw <alfred.91.mc@gmail.com>
2023-01-11 18:12:23 +0100

    1101

---
+++ Tema4/src/empleado/Aplicacion.java
@@ -0,0 +1,13 @@
+package empleado;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+Empleado e1 =new Empleado("Manolo","CabezaBolo",33,"holasoyeltomate@gmail.com",2000,0.22,0.06,"RRHH");
+
+System.out.println("Salario : "+e1.getSalarioMensualNeto());
+System.out.println(e1);
+
+	}
+
+}
