a4fb184e7f34decb27a84aaab41c88c6e4cb8ac8
Apache <alfred.91.mc@gmail.com>
2023-01-30 18:18:18 +0100

    tema4

--- Tema4/src/empleado/Aplicacion.java
+++ Tema4/src/empleado/Aplicacion.java
@@ -3,11 +3,17 @@ package empleado;
 public class Aplicacion {
 
 	public static void main(String[] args) {
-Empleado e1 =new Empleado("Manolo","CabezaBolo",33,"holasoyeltomate@gmail.com",2000,0.22,0.06,"RRHH");
+Empleado e1 =new Empleado("Manolo","CabezaBolo",33,"holasoyeltomate@gmail.com",2000,0.22,0.06,0, "RRHH");
 
 System.out.println("Salario : "+e1.getSalarioMensualNeto());
 System.out.println(e1);
 
+	}
+
+	@Override
+	public String toString() {
+		return "Aplicacion [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
 	}
 
 }
