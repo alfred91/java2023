e16118da6fbc55c3b6f3eafa58d5f746b114fa64
Apache <alfred.91.mc@gmail.com>
2023-02-08 03:00:32 +0100

    .

--- Practica6/src/ejercicio7/SteamJuego.java
+++ Practica6/src/ejercicio7/SteamJuego.java
@@ -1,5 +1,69 @@
 package ejercicio7;
 
 public class SteamJuego {
+	
+String nombre,descripcionCorta,imagen,freeToPlay;
+int mediaJugadoresDiarios,diasDeSalida,precio,puestoMasVendidos;
 
+public SteamJuego(String nombre, String descripcionCorta,String imagen, int mediaJugadoresDiarios, int diasDeSalida,
+		int precio, String freeToPlay, int puestoMasVendidos) {
+	super();
+	this.nombre = nombre;
+	this.descripcionCorta = descripcionCorta;
+	this.imagen = imagen;
+	this.mediaJugadoresDiarios = mediaJugadoresDiarios;
+	this.diasDeSalida = diasDeSalida;
+	this.precio = precio;
+	this.puestoMasVendidos = puestoMasVendidos;
+	this.freeToPlay=freeToPlay;
 }
+
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getDescripcionCorta() {
+	return descripcionCorta;
+}
+public void setDescripcionCorta(String descripcionCorta) {
+	this.descripcionCorta = descripcionCorta;
+}
+public String getImagen() {
+	return imagen;
+}
+public void setImagen(String imagen) {
+	this.imagen = imagen;
+}
+public int getMediaJugadoresDiarios() {
+	return mediaJugadoresDiarios;
+}
+public void setMediaJugadoresDiarios(int mediaJugadoresDiarios) {
+	this.mediaJugadoresDiarios = mediaJugadoresDiarios;
+}
+public int getDiasDeSalida() {
+	return diasDeSalida;
+}
+public void setDiasDeSalida(int diasDeSalida) {
+	this.diasDeSalida = diasDeSalida;
+}
+public String freeToPlay() {
+	if (this.precio==0) { return "Free to play!";} else {return "€";} 
+}
+public void freeToPlay(String freeToPlay) {
+	this.freeToPlay = freeToPlay;
+}
+public int getPrecio() {
+	return precio;
+}
+public void setPrecio(int precio) {
+	this.precio = precio;
+}
+public int getPuestoMasVendidos() {
+	return puestoMasVendidos;
+}
+public void setPuestoMasVendidos(int puestoMasVendidos) {
+	this.puestoMasVendidos = puestoMasVendidos;
+}}
+//*public void jugadoresDia(int jugadoresHoy) {((this.jugadoresDia(jugadoresHoy)*this.diasDeSalida)+this.jugadoresDia(jugadoresHoy)/(this.diasDeSalida);
