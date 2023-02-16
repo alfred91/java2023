9e34e028c8e4a58c8136a75f3f298d6876599389
usario1daw <alfred.91.mc@gmail.com>
2023-02-16 19:29:35 +0100

    1

--- Ahorcado/src/ahorcado/Main.java
+++ Ahorcado/src/ahorcado/Main.java
@@ -2,44 +2,6 @@ package ahorcado;
 import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		
-		String palabraSecreta=damePalabraSecreta();
-			char [] palabraGuiones=dameGuionesDePalabra(palabraSecreta);
-			boolean juegoTerminado=false;
-			int intentos=7;
-			Scanner sc = new Scanner(System.in);
-		
-			do {
-				System.out.println("Te quedan "+intentos+" intentos");
-				System.out.println(palabraGuiones);
-				System.out.println("Introduce una letra: ");
-				char letra= sc.next().charAt(0);
-				boolean algunaLetraAcertada=false;
-					for (int i=0;i<palabraSecreta.length();i++) {
-						if (palabraSecreta.charAt(i)==letra) {
-								palabraGuiones[i]=letra;
-									algunaLetraAcertada=true;
-					}
-				}
-					
-			if(!algunaLetraAcertada) {
-				System.out.println("No has acertado "); --intentos;}
-						
-			else {--intentos; System.out.println("Correcto ");}
-			if (intentos==0) {System.out.println("Has agotado los intentos");
-				juegoTerminado=true;
-				}
-					else {
-						boolean juegoGanado=!hayGuiones(palabraGuiones);
-							if(juegoGanado) {System.out.println("Has ganado!!");
-							juegoTerminado=true;}
-					}
-		
-				} while (!juegoTerminado);
-					sc.close();
-			}
 	
 		static String damePalabraSecreta() {
 			
@@ -49,7 +11,7 @@ public class Main {
 					
 						return palabras[n];
 		}
-	
+		
 	static char[] dameGuionesDePalabra(String palabra) {
 
 		int nLetrasPalabraSecreta= palabra.length();
--- Ahorcado/src/ahorcado/TestAhorcado.java
+++ Ahorcado/src/ahorcado/TestAhorcado.java
@@ -1,9 +1,45 @@
 package ahorcado;
 
+import java.util.Scanner;
+
 public class TestAhorcado {
+	
 
-	public static void main(String[] args) {
-
-	}
-
+public static void main(String[] args) {
+		
+		String palabraSecreta=Main.damePalabraSecreta();
+			char [] palabraGuiones=Main.dameGuionesDePalabra(palabraSecreta);
+			boolean juegoTerminado=false;
+			int intentos=7;
+			Scanner sc = new Scanner(System.in);
+		
+			do {
+				System.out.println("Te quedan "+intentos+" intentos");
+				System.out.println(palabraGuiones);
+				System.out.println("Introduce una letra: ");
+				char letra= sc.next().charAt(0);
+				boolean algunaLetraAcertada=false;
+					for (int i=0;i<palabraSecreta.length();i++) {
+						if (palabraSecreta.charAt(i)==letra) {
+								palabraGuiones[i]=letra;
+									algunaLetraAcertada=true;
+					}
+				}
+					
+			if(!algunaLetraAcertada) {
+				System.out.println("No has acertado "); --intentos;}
+						
+			else {--intentos; System.out.println("Correcto ");}
+			if (intentos==0) {System.out.println("Has agotado los intentos");
+				juegoTerminado=true;
+				}
+					else {
+						boolean juegoGanado=!Main.hayGuiones(palabraGuiones);
+							if(juegoGanado) {System.out.println("Has ganado!!");
+							juegoTerminado=true;}
+					}
+		
+				} while (!juegoTerminado);
+					sc.close();
+			}
 }
