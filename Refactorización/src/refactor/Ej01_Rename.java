04ff109e79f23b8a7f6dae8f898537ca8986f2d4
Apache <alfred.91.mc@gmail.com>
2023-03-07 20:24:56 +0100

    1

---
+++ Refactorización/src/refactor/Ej01_Rename.java
@@ -0,0 +1,13 @@
+package refactor;
+
+public class Ej01_Rename {
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 13;
+		int x = 0;
+		x = a + b;
+		System.out.println("El número de alumnos es " + a);
+		System.out.println("El número de alumnas es " + b);
+		System.out.println("El total de matrículas es " + x);
+	}
+}
