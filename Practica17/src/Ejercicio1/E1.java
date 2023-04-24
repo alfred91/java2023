c0db6a340f21ae077337681d80c340fba78b3ff9
usario1daw <alfred.91.mc@gmail.com>
2023-04-24 21:49:40 +0200

    1

---
+++ Practica17/src/Ejercicio1/E1.java
@@ -0,0 +1,26 @@
+package Ejercicio1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class E1 {
+	
+	public static void main(String[] args) {
+		
+        List<String> lista = Arrays.asList("The", "Quick", "BROWN", "Fox", "Jumped", "Over", "The", "LAZY", "DOG", "In", "THE", "Street");
+
+        List<String> listaMinusculas = lista.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+
+        System.out.println("Minusculas "+listaMinusculas);
+        
+        List<String> listaMasDeTres = lista.stream()
+                .filter(s -> s.length() > 3)
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+
+        System.out.println("Mas de tres " +listaMasDeTres);
+    }
+}
