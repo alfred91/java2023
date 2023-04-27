abc8a1ce7aad368f0e06b42215bc60be386adc0e
usario1daw <alfred.91.mc@gmail.com>
2023-04-27 20:36:54 +0200

    1

--- Practica10/src/ejercicio3/Ejecutable.java
+++ Practica10/src/ejercicio3/Ejecutable.java
@@ -9,9 +9,9 @@ public class Ejecutable {
 		ArrayList<Serie> series = new ArrayList<>();
 		ArrayList<Videojuego> videojuegos = new ArrayList<>();
 		
-		series.add(new Serie("The Last of us", 1, "Un fulano", Serie.Genero.DRAMA));
-		series.add(new Serie("Tokyo Vice", 1, "Otro fulano", Serie.Genero.DRAMA));
-		series.add(new Serie("Friends", 7, "Friendzone", Serie.Genero.COMEDIA));
+		series.add(new Serie("The Last of us", 1, "Un fulano", "Drama"));
+		series.add(new Serie("Tokyo Vice", 1, "Otro fulano", "Comedia"));
+		series.add(new Serie("Friends", 7, "Friendzone", "Suspense"));
 		
 		videojuegos.add(new Videojuego("Fifa 23", 200, Videojuego.GeneroV.DEPORTES, "EA"));
 		videojuegos.add(new Videojuego("Valorant", 300, Videojuego.GeneroV.SHOOTER, "Riot Games"));
--- Practica10/src/ejercicio3/Entregable.java
+++ Practica10/src/ejercicio3/Entregable.java
@@ -2,17 +2,11 @@ package ejercicio3;
 
 public interface Entregable {
 	
-
 	void entregar();
 	
 	void devolver();
 	
-	boolean EsPrestado();
 
-	public void entregar();
-	
-	public void devolver();
-	
-	public boolean esPrestado();
+	boolean esPrestado();
 	
 }
