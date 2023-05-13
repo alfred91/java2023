3982d767759244469d011dbfd3fb7d2f921938f2
Apache <alfred.91.mc@gmail.com>
2023-05-13 02:48:43 +0200

    1

--- Practica17/src/Ejercicio2/TestPelicula.java
+++ Practica17/src/Ejercicio2/TestPelicula.java
@@ -1,9 +1,10 @@
 package Ejercicio2;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.concurrent.*;
 
 public class TestPelicula {
 	
