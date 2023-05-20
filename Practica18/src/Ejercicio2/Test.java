7847c7649d1d922eb3b69344eed8cda86c4ee105
Apache <alfred.91.mc@gmail.com>
2023-05-20 14:38:46 +0200

    1

--- Practica18/src/Ejercicio2/Test.java
+++ Practica18/src/Ejercicio2/Test.java
@@ -9,30 +9,45 @@ public class Test {
 	
     public static void main(String[] args) {
     	
-        try {
-        	
-            String directoryPath = "C:\\Users\\usario1daw\\test";
-            long directorySize = FileUtils.calculateDirectorySize(directoryPath);
-            System.out.println("El tamanio total del directorio es : " + directorySize + " bytes");
 
-            String searchDirectoryPath = "C:\\Users\\usario1daw\\test";
-            String fileNameToSearch = "Hola.txt Hola2.txt";
-            System.out.println("Archivos Encontrados : "+fileNameToSearch+" ");
-            FileUtils.searchFileInSubdirectories(searchDirectoryPath, fileNameToSearch);
+    	try {
+            String currentDirectory = Paths.get("").toAbsolutePath().toString();
 
-            String deleteDirectoryPath = "C:\\Users\\usario1daw\\test";
-            String fileNameToDelete = "Borrar.txt";
-            FileUtils.deleteFileInDirectory(deleteDirectoryPath, fileNameToDelete);
-            System.out.println("Archivos eliminados con Exito");
+            //CALCULAR TAMANIO DEL DIRECTORIO
+            
+            long directorySize = FileUtils.calculateDirectorySize(currentDirectory);
+            System.out.println("Tamaño total del directorio: " + directorySize + " bytes");
 
-            String regularFilesDirectoryPath = "C:\\Users\\usario1daw\\test";
-            List<Path> regularFiles = FileUtils.getRegularFilesInDirectory(regularFilesDirectoryPath);
-            System.out.println("Archivos regulares encontrados : ");
+            //LISTAR ARCHIVOS REGULARES
+            
+            System.out.println("Archivos regulares en el directorio:");
+            List<Path> regularFiles = FileUtils.getRegularFilesInDirectory(currentDirectory);
             for (Path file : regularFiles) {
                 System.out.println(file);
             }
+
+            //BUSCAR ARCHIVO
+            
+            String searchFileName = "archivo.txt";
+            System.out.println("Búsqueda del archivo '" + searchFileName + "' en subdirectorios:");
+            FileUtils.searchFileInSubdirectories(currentDirectory, searchFileName);
+
+            //ELIMINAR
+            
+            String deleteFileName = "archivo.txt";
+            System.out.println("Eliminación del archivo '" + deleteFileName + "' en el directorio y subdirectorios:");
+            FileUtils.deleteFileInDirectory(currentDirectory, deleteFileName);
+
+            // AGREGAR
+            
+            String regularFilesDirectoryPath = "C:\\Users\\Apache\\eclipse-workspace\\Practica18";
+            List<Path> additionalRegularFiles = FileUtils.getRegularFilesInDirectory(regularFilesDirectoryPath);
+            System.out.println("Archivos regulares encontrados en '" + regularFilesDirectoryPath + "':");
+            for (Path file : additionalRegularFiles) {
+                System.out.println(file);
+            }
         } catch (IOException e) {
-            System.err.println("Error al realizar las operaciones de archivo: " + e.getMessage());
+            System.out.println("Error al realizar la operacion: " + e.getMessage());
         }
     }
 }
